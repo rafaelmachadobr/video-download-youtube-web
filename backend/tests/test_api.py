a8f01fee9d7a7266d347a97d2b5de94ac468333c
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-11-29 17:39:01 -0300

    feat: add test cases for video info route

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -14,6 +14,9 @@ BASE_URL = "http://127.0.0.1:8000/"
 
 @pytest.mark.asyncio
 async def test_get_video_info_route():
+    """
+    Testa a rota para obter informações sobre um vídeo do YouTube.
+    """
     video_id = "S9uPNppGsGo"
     url = f"/video/info/{video_id}"
 
@@ -30,6 +33,9 @@ async def test_get_video_info_route():
 
 @pytest.mark.asyncio
 async def test_get_video_info_route_with_invalid_id():
+    """
+    Testa a rota com um ID de vídeo inválido.
+    """
     invalid_video_id = "invalid_id"
     url = f"/video/info/{invalid_video_id}"
 
@@ -38,12 +44,16 @@ async def test_get_video_info_route_with_invalid_id():
 
     assert response.status_code == 500
     assert response.json() == {
-        "detail": f"Erro ao obter informações do vídeo: regex_search: could not find match for "
-                  f"(?:v=|\\/)([0-9A-Za-z_-]{{11}}).*"}
+        "detail": "Erro ao obter informações do vídeo: regex_search: could not find match for "
+        "(?:v=|\\/)([0-9A-Za-z_-]{11}).*"
+    }
 
 
 @pytest.mark.asyncio
 async def test_get_video_info_route_with_unavailable_video():
+    """
+    Testa a rota com um ID de vídeo indisponível.
+    """
     unavailable_video_id = "unavailable_id"
     url = f"/video/info/{unavailable_video_id}"
 
@@ -51,22 +61,31 @@ async def test_get_video_info_route_with_unavailable_video():
         response = await ac.get(url)
 
     assert response.status_code == 404
-    assert response.json() == {"detail": "Vídeo não disponível: unavailable is unavailable"}
+    assert response.json() == {
+        "detail": "Vídeo não disponível: unavailable is unavailable"}
 
 
 @pytest.mark.asyncio
 async def test_get_video_info_route_with_invalid_url():
+    """
+    Testa a rota com uma URL de vídeo inválida.
+    """
     invalid_url = "/video/info/invalid_url"
 
     async with AsyncClient(app=app, base_url=BASE_URL) as ac:
         response = await ac.get(invalid_url)
 
     assert response.status_code == 404
-    assert response.json() == {"detail": f"Vídeo não disponível: invalid_url is unavailable"}
+    assert response.json() == {
+        "detail": "Vídeo não disponível: invalid_url is unavailable"
+    }
 
 
 @pytest.mark.asyncio
 async def test_get_video_info_route_with_invalid_method():
+    """
+    Testa a rota com um método HTTP inválido.
+    """
     invalid_url = "/video/info/invalid_url"
 
     async with AsyncClient(app=app, base_url=BASE_URL) as ac:
@@ -78,6 +97,9 @@ async def test_get_video_info_route_with_invalid_method():
 
 @pytest.mark.asyncio
 async def test_get_video_info_route_with_invalid_path():
+    """
+    Testa a rota com um caminho inválido.
+    """
     invalid_url = "/video/invalid_path/invalid_url"
 
     async with AsyncClient(app=app, base_url=BASE_URL) as ac:
