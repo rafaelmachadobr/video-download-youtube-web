77c813178b1bd9c40c94cc75fc2c4672756eb3e8
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-11-30 15:01:49 -0300

    feat: add custom colors and font family to tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3490dc",
+        secondary: "#6574cd",
+        accent: "#38c172",
+        error: "#e3342f",
+        black: "#222222",
+      },
+    },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
   },
   plugins: [],
 };
