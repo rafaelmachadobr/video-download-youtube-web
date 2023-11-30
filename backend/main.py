0f0ff81bb0219b44757124d803e74acc50a8f081
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-11-30 16:23:31 -0300

    feat: add CORS middleware to FastAPI app

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api import router as router_app
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 
 app.include_router(router_app, prefix="/api")
